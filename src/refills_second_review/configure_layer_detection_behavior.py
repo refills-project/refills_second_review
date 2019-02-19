c35470d21996aa8f912ef5e88231bb0f07c3d14f
REFILLS user on Donbot <all@refills.eu>
2019-02-19 16:45:13 +0100

    Finished implementation of shelf detection.

--- src/refills_second_review/configure_layer_detection_behavior.py
+++ src/refills_second_review/configure_layer_detection_behavior.py
@@ -2,12 +2,14 @@ from refills_msgs.srv import QueryDetectShelfLayersPath, QueryDetectShelfLayersP
 from py_trees import Behaviour, Status, Blackboard
 import rospy
 from rospy import ROSException
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
 
 class ConfigureLayerDetectionBehavior(Behaviour):
     def __init__(self, name="", *args, **kwargs):
         super(ConfigureLayerDetectionBehavior, self).__init__(name, *args, **kwargs)
         self.client = None # type: rospy.ServiceProxy
+        self.trajectory_sample_period = None # type: float
 
     def setup(self, timeout):
         self.client = rospy.ServiceProxy('/perception_interface/query_detect_shelf_layers_path', QueryDetectShelfLayersPath)
@@ -15,6 +17,10 @@ class ConfigureLayerDetectionBehavior(Behaviour):
             self.client.wait_for_service(timeout)
         except ROSException as e:
             return False
+        if not rospy.has_param('~trajectory_sample_period'):
+            self.feedback_message = "Could not find ROS param '~trajectory_sample_period'"
+            return False
+        self.trajectory_sample_period = rospy.get_param('~trajectory_sample_period')
         return super(ConfigureLayerDetectionBehavior, self).setup(timeout)
 
     def initialise(self):
@@ -31,8 +37,25 @@ class ConfigureLayerDetectionBehavior(Behaviour):
             return Status.FAILURE
 
         Blackboard().set("move_base_target_pose", response.path.postures[0].base_pos)
-        # TODO: set arm_trajectory of type trajectory_msgs/JointTrajectory
+        Blackboard().set("arm_trajectory", self._extract_trajectory(response))
         return Status.SUCCESS
 
     def terminate(self, new_status):
         super(ConfigureLayerDetectionBehavior, self).terminate(new_status)
+
+    def _extract_trajectory(self, response):
+        """
+        Extracts a trajectory message from the response of a call to query_detect_shelf_layers_path.
+        :param response: The response from the service call.
+        :type reponse: QueryDetectShelfLayersPathResponse
+        :return: The extracted trajectory.
+        :rtype: JointTrajectory
+        """
+        trajectory = JointTrajectory()
+        trajectory.joint_names = [joint.name for joint in response.path.postures[0].joints]
+        for count, posture in enumerate(response.path.postures):
+            p = JointTrajectoryPoint()
+            p.time_from_start = rospy.Duration((count+1)*self.trajectory_sample_period)
+            p.positions = [joint.position for joint in posture.joints]
+            trajectory.points.append(p)
+        return trajectory
