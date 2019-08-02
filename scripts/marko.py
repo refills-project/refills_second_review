b32d47d65d76d3edf75b1bfa5d9fec8994bae1f6
Simon Stelter <stelter@tzi.de>
2019-08-02 15:10:14 +0200

    added shelf experiment to marko.py

--- scripts/marko.py
+++ scripts/marko.py
@@ -1,5 +1,6 @@
 import json
 import numpy as np
+from copy import deepcopy
 from itertools import product
 
 import rospy
@@ -24,7 +25,7 @@ class Plan(object):
         self.without_base = 'base_footprint'
         self.with_base = 'odom'
         self.tip = 'refills_tool_frame'
-        self.gripper = Gripper(True)
+        self.gripper = Gripper(False)
         self.giskard = GiskardWrapper()
         rospy.sleep(1)
         self.giskard.clear_world()
@@ -46,6 +47,7 @@ class Plan(object):
         p.header.frame_id = 'map'
         p.pose.position.x = x
         p.pose.position.y = y
+        p.pose.position.z = 0
         p.pose.orientation = Quaternion(*quaternion_about_axis(rot, [0, 0, 1]))
         self.giskard.set_cart_goal(self.with_base, self.without_base, p)
         self.giskard.plan_and_execute()
@@ -75,19 +77,21 @@ class Plan(object):
 
     def add_shelf(self):
         back = 'back'
-        p = PoseStamped()
-        p.header.frame_id = 'map'
-        p.pose.position.x = 4
-        p.pose.position.y = -1.5
-        p.pose.position.z = 1
-        p.pose.orientation = Quaternion(*quaternion_about_axis(np.pi / 2, [0, 0, 1]))
-        self.giskard.add_box(back, size=[1, 0.05, 2], pose=p)
+        back_p = PoseStamped()
+        back_p.header.frame_id = 'map'
+        back_p.pose.position.x = 4
+        back_p.pose.position.y = -1.5
+        back_p.pose.position.z = 1
+        back_p.pose.orientation = Quaternion(*quaternion_about_axis(np.pi / 2, [0, 0, 1]))
+        self.giskard.add_box(back, size=[1, 0.05, 2], pose=back_p)
 
+        p = deepcopy(back_p)
         for i, height in enumerate([0.2, 0.6, 0.93, 1.31]):
             layer1 = 'layer{}'.format(i)
             p.pose.position.x = 3.75
             p.pose.position.z = height
             self.giskard.add_box(layer1, size=[1, 0.5, 0.02], pose=p)
+        return back_p
 
     def add_tulip_on_floor(self, name='tulip'):
         box_pose = PoseStamped()
@@ -114,7 +118,7 @@ class Plan(object):
         else:
             print('grasp angle {} expected {}'.format(js, angle))
 
-    def allow_base_y_movement(self, y, rot):
+    def allow_base_y_movement(self, rot):
         p = lookup_pose(self.without_base, self.with_base)
         self.giskard.set_json_goal('CartesianPositionY', **{'root_link': self.without_base,
                                                             'tip_link': self.with_base,
@@ -153,8 +157,6 @@ class Plan(object):
         arm_goal.pose.position.z = 0.07
         self.giskard.set_translation_goal(self.with_base, 'refills_finger', arm_goal, max_speed=self.translation_limit)
         self.giskard.set_rotation_goal(self.with_base, 'refills_finger', arm_goal, max_speed=self.rotation_limit)
-        # js = self.get_joint_state()
-        # js = {k:v for k,v in js.items() if k in ['odom_']}
         self.allow_base_y_movement(3.1, -1.57)
         self.giskard.allow_collision([CollisionEntry.ALL], tulip, [CollisionEntry.ALL])
         if start_angle is not None:
@@ -164,9 +166,9 @@ class Plan(object):
             print('no solution found; stopping test')
             return 'start'
 
-        raw_input('press a key')
+        # raw_input('press a key')
         self.gripper.grasp(5)
-        raw_input('press a key')
+        # raw_input('press a key')
         self.giskard.attach_object(tulip, 'refills_finger')
 
         # place object
@@ -177,12 +179,94 @@ class Plan(object):
         self.giskard.set_translation_goal(self.with_base, tulip, goal_pose, max_speed=self.translation_limit)
         self.giskard.set_rotation_goal(self.with_base, tulip, goal_pose, max_speed=self.rotation_limit)
         self.allow_base_y_movement(3.1, -1.57)
-        # self.giskard.allow_collision(['gripper_base_link'], 'table', [CollisionEntry.ALL])
         if goal_angle is not None:
             self.giskard.set_joint_goal({'refills_finger_joint': goal_angle})
         self.keep_horizontal(tulip)
         if not self.giskard.plan_and_execute().error_code == MoveResult.SUCCESS or \
                 not self.goal_reached(goal_pose, tulip, goal_angle):
+            print('no solution found; stopping test')
+            return 'end'
+        self.gripper.home()
+        return 'ok'
+
+    def pick_up_shelf(self, start_angle=0, goal_angle=0, height=0.72):
+        if start_angle is not None:
+            start_angle -= np.pi / 2
+        if goal_angle is not None:
+            goal_angle -= np.pi / 2
+        tulip = 'tulip'
+
+        # reset
+        self.gripper.home()
+        self.add_table(height=height)
+        base_pose = self.add_shelf()
+        base_pose.pose.position.x -= 2
+        base_pose.pose.position.y -= 0.3
+        base_pose.pose.position.z = 0
+        base_pose.pose.orientation = Quaternion(*quaternion_about_axis(np.pi, [0,0,1]))
+        self.giskard.set_cart_goal(self.with_base, self.without_base, base_pose, trans_max_speed=0.2)
+        self.giskard.plan_and_execute()
+
+        # spawn tulip
+        base_pose.pose.position.x += 1
+        base_pose.pose.position.y += 0.35
+        base_pose.pose.orientation = Quaternion(*quaternion_about_axis(-np.pi/2, [0,0,1]))
+        base_pose.pose.position.z = 0.045  # box height
+        self.giskard.add_box(tulip, [0.05, 0.095, 0.09], pose=base_pose)
+
+        self.start_config()
+        planning_time_recorder.reset()
+
+        # grasp tulip
+        arm_goal = deepcopy(base_pose)
+        o_R_g = np_to_kdl(np.array([[1, 0, 0, 0],
+                                    [0, 0, -1, 0],
+                                    [0, 1, 0, 0],
+                                    [0, 0, 0, 1]]))
+        m_R_o = msg_to_kdl(arm_goal)
+        m_R_g = m_R_o.M * o_R_g.M
+        arm_goal.pose.orientation = kdl_to_quaternion(m_R_g)
+        arm_goal.pose.position.z = 0.07
+        self.giskard.set_translation_goal(self.with_base, 'refills_finger', arm_goal, max_speed=self.translation_limit)
+        self.giskard.set_rotation_goal(self.with_base, 'refills_finger', arm_goal, max_speed=self.rotation_limit)
+        self.allow_base_y_movement(np.pi)
+        self.giskard.allow_collision([CollisionEntry.ALL], tulip, [CollisionEntry.ALL])
+        if start_angle is not None:
+            self.giskard.set_joint_goal({'refills_finger_joint': start_angle}, max_speed=self.rotation_limit)
+        if not self.giskard.plan_and_execute().error_code == MoveResult.SUCCESS or \
+                not self.goal_reached(arm_goal, 'refills_finger', start_angle):
+            print('no solution found; stopping test')
+            return 'start'
+
+        # raw_input('press a key')
+        self.gripper.grasp(5)
+        # raw_input('press a key')
+        self.giskard.attach_object(tulip, 'refills_finger')
+
+        # place object
+        goal1 = deepcopy(arm_goal)
+        goal1.pose.position.z = height+ 0.035
+        goal1.pose.position.x += 0.2
+        goal1.pose.orientation = deepcopy(base_pose.pose.orientation)
+        self.giskard.set_translation_goal(self.with_base, tulip, goal1, max_speed=self.translation_limit)
+        self.giskard.set_rotation_goal(self.with_base, tulip, goal1, max_speed=self.rotation_limit)
+        self.allow_base_y_movement(np.pi)
+        self.keep_horizontal(tulip)
+
+        self.giskard.add_cmd()
+        goal2 = deepcopy(goal1)
+        goal2.pose.position.x += 0.4
+        goal2.pose.position.z += 0.045
+        self.giskard.set_translation_goal(self.with_base, tulip, goal2, max_speed=self.translation_limit)
+        self.giskard.set_rotation_goal(self.with_base, tulip, goal2, max_speed=self.rotation_limit)
+        self.allow_base_y_movement(np.pi)
+        self.keep_horizontal(tulip)
+
+        if goal_angle is not None:
+            self.giskard.set_joint_goal({'refills_finger_joint': goal_angle})
+        self.keep_horizontal(tulip)
+        if not self.giskard.plan_and_execute().error_code == MoveResult.SUCCESS or \
+                not self.goal_reached(goal2, tulip, goal_angle):
             print('no solution found; stopping test')
             return 'end'
         self.gripper.home()
@@ -226,32 +310,65 @@ def make_entry(start_angle, goal_angle, success, planning_time, execution_time, 
     }
 
 
-rospy.init_node('markos_demo')
+def pick_and_place_table():
+    data = []
 
-data = []
+    plan = Plan()
+    failed_starts = set()
+    start_angles = np.arange(-np.pi / 2, np.pi / 2, np.pi / 4).tolist() + [np.pi / 2, None]
+    goal_angles = np.arange(-np.pi / 2, np.pi / 2, np.pi / 4).tolist() + [np.pi / 2, None]
+    # table_heights = [0.2, 0.6, 0.72, 0.93, 1.31]
+    table_heights = [0.72]
 
-plan = Plan()
-failed_starts = set()
-planning_time_recorder = PlanningTimeRecorder()
-start_angles = np.arange(-np.pi / 2, np.pi / 2, np.pi / 4).tolist() + [np.pi / 2, None]
-goal_angles = np.arange(-np.pi / 2, np.pi / 2, np.pi / 4).tolist() + [np.pi / 2, None]
-# table_heights = [0.2, 0.6, 0.72, 0.93, 1.31]
-table_heights = [0.72]
+    # combinations = [[-np.pi/4, ]]
 
-# combinations = [[-np.pi/4, ]]
+    with open('log.json2', 'r') as f:
+        matrix = json.load(f)
 
-with open('log.json2', 'r') as f:
-    matrix = json.load(f)
+    skip = True
+    # for start_angle, goal_angle, table_height in product(start_angles, goal_angles, table_heights):
+    # for start_angle, goal_angle, table_height in [(-np.pi/4, np.pi/4, 0.72)]:
+    for entry in matrix:
+        start_angle = entry['start_angle']
+        goal_angle = entry['goal_angle']
+        if entry['success'] and (goal_angle is None or not np.isclose(np.array(goal_angle), np.array(np.pi/4))):
+            table_height = entry['table_height']
+            print('executing {} {} {}'.format(start_angle, goal_angle, table_height))
+            if start_angle == np.pi/4 and goal_angle == 0.0:
+                skip = False
+            if skip:
+                continue
+            planning_time_recorder.reset()
+            result = ''
+            if start_angle not in failed_starts:
+                plan.giskard.clear_world()
+                result = plan.pick_up(start_angle, goal_angle, table_height)
+                if result == 'start':
+                    failed_starts.add(start_angle)
+            js = plan.get_joint_state()['refills_finger_joint'] + np.pi/2
+            entry = make_entry(start_angle, goal_angle, result == 'ok', planning_time_recorder.planning_time,
+                               planning_time_recorder.traj_length, table_height, js)
 
-skip = True
-# for start_angle, goal_angle, table_height in product(start_angles, goal_angles, table_heights):
-# for start_angle, goal_angle, table_height in [(-np.pi/4, np.pi/4, 0.72)]:
-for entry in matrix:
-    start_angle = entry['start_angle']
-    goal_angle = entry['goal_angle']
-    if entry['success'] and (goal_angle is None or not np.isclose(np.array(goal_angle), np.array(np.pi/4))):
-        table_height = entry['table_height']
-        print('executing {} {} {}'.format(start_angle, goal_angle, table_height))
+            print(entry)
+            print('-----------------------------')
+            data.append(entry)
+            with open('log_real4.json', 'w') as f:
+                json.dump(data, f, indent=4, sort_keys=True)
+
+def pick_and_place_shelf():
+    data = []
+
+    plan = Plan()
+    failed_starts = set()
+    start_angles = np.arange(-np.pi / 4, np.pi / 4, np.pi / 4).tolist() + [np.pi / 2, None]
+    goal_angles = np.arange(-np.pi / 2, 0, np.pi / 4).tolist() + [np.pi / 2, None]
+    table_heights = [0.2, 0.6, 0.93, 1.31]
+    # table_heights = [0.72]
+
+    skip = False
+    for start_angle, goal_angle, height in product(start_angles, goal_angles, table_heights):
+    # for start_angle, goal_angle, table_height in [(-np.pi/4, np.pi/4, 0.72)]:
+        print('executing {} {} {}'.format(start_angle, goal_angle, height))
         if start_angle == np.pi/4 and goal_angle == 0.0:
             skip = False
         if skip:
@@ -260,15 +377,21 @@ for entry in matrix:
         result = ''
         if start_angle not in failed_starts:
             plan.giskard.clear_world()
-            result = plan.pick_up(start_angle, goal_angle, table_height)
+            result = plan.pick_up_shelf(start_angle, goal_angle, height)
             if result == 'start':
                 failed_starts.add(start_angle)
         js = plan.get_joint_state()['refills_finger_joint'] + np.pi/2
         entry = make_entry(start_angle, goal_angle, result == 'ok', planning_time_recorder.planning_time,
-                           planning_time_recorder.traj_length, table_height, js)
+                           planning_time_recorder.traj_length, height, js)
 
         print(entry)
         print('-----------------------------')
         data.append(entry)
-        with open('log_real4.json', 'w') as f:
+        with open('log_shelf.json', 'w') as f:
             json.dump(data, f, indent=4, sort_keys=True)
+
+
+rospy.init_node('markos_demo')
+planning_time_recorder = PlanningTimeRecorder()
+pick_and_place_shelf()
+
