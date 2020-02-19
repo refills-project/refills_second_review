f8e7946b272aacf22cda2d224dbdd2c883b10d25
REFILLS user on Donbot <all@refills.eu>
2020-02-19 10:56:15 +0100

    resolved merge conflict

---
+++ scripts/post_process_augsburg.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import rospy
+import rospkg
+
+from arrina_rooming_in.post_processing import PostProcessing
+
+if __name__ == '__main__':
+    # setup
+    rospy.init_node('kb')
+    # path_to_json = '{}/data/donbot_bremen/19_shelves.json'.format(rospkg.RosPack().get_path('refills_second_review'))
+    filename = '{}/data/donbot_augsburg/pp_scan'.format(rospkg.RosPack().get_path('refills_second_review'))
+    my_pp = PostProcessing('', filename)
+
+    # putting missing properties
+    my_pp.assert_missing_shelf_props(43, 2.0, 5, "light")
+    my_pp.assert_missing_shelf_props(35, 2.0, 5, "light")
+    # my_pp.assert_missing_shelf_props(15, 2.0, 5, "light")
+    # my_pp.assert_missing_shelf_props(17, 2.0, 5, "light")
+
+
+    # flusing out
+    my_pp.correct_shelf_poses()
+    my_pp.export_owl()
