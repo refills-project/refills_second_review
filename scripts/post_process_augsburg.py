c665a80588516a0b92336bd7ab8d4649cac5340d
ARRINA user on robot <ai-arrina@cs.uni-bremen.de>
2020-11-17 14:03:51 +0100

    added launch files for kmr sim

--- scripts/post_process_augsburg.py
+++ scripts/post_process_augsburg.py
@@ -11,14 +11,17 @@ if __name__ == '__main__':
     # path_to_json = '{}/data/donbot_bremen/19_shelves.json'.format(rospkg.RosPack().get_path('refills_second_review'))
     filename = '{}/data/donbot_augsburg/pp_scan'.format(rospkg.RosPack().get_path('refills_second_review'))
     my_pp = PostProcessing('', filename)
+    my_pp.kb.start_tf_logging()
 
     # putting missing properties
     my_pp.assert_missing_shelf_props(43, 2.0, 5, "light")
     my_pp.assert_missing_shelf_props(35, 2.0, 5, "light")
-    # my_pp.assert_missing_shelf_props(15, 2.0, 5, "light")
+    my_pp.assert_missing_shelf_props(33, 1.6, 5, "light")
     # my_pp.assert_missing_shelf_props(17, 2.0, 5, "light")
 
 
     # flusing out
     my_pp.correct_shelf_poses()
+    rospy.sleep(2)
+    my_pp.kb.stop_tf_logging()
     my_pp.export_owl()
